da03a4c1e2c7e50922c8302bbbb31b5046082f94
Caique Murilo Sacramento <caiquemurilo@gmail.com>
2022-06-29 00:18:47 -0300

    retificacao responsividade box textarea

--- assets/javascript/arquivo1.js
+++ assets/javascript/arquivo1.js
@@ -1,16 +1,17 @@
 function resizeTextarea() {
-  let screensize = screen.width;
-  let textArea = document.getElementById('message');
-  if(screensize <= 768) {
-    textArea.setAttribute("cols", "40")
+  let screensize = screen.width
+  let textArea = document.getElementById('message')
+  if (screensize <= 576) {
+    textArea.setAttribute('cols', '30')
+  } else if (screensize <= 768) {
+    textArea.setAttribute('cols', '40')
   } else {
-    textArea.setAttribute("cols", "60")
+    textArea.setAttribute('cols', '60')
   }
 }
 
-
-window.onload = function() {
+window.onload = function () {
   resizeTextarea()
 }
 
-window.addEventListener("resize", resizeTextarea);
+window.addEventListener('resize', resizeTextarea)
